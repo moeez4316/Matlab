b8b6092a0bc80a0e29f44771448323ebaf28890f
Moeez Ahmed <126664082+moeez4316@users.noreply.github.com>
2025-02-28 12:46:16 +0500

    modify basic codes

--- basic_code.m
+++ basic_code.m
@@ -2,10 +2,10 @@ arr = [10 11 12 13; 14 15 16 17; 18 19 20 21];
 arr (1:2:3, 3);
 arr (1:1:3, 3:-1:1);
 
-% Read the image
+// Read the image
 img = imread('C:\Users\Moeez Ahmad\Pictures\Saved Pictures\backiee-36257.jpg');
 
-% Convert 8-bit values to decimal (It's already in decimal in MATLAB)
+//Convert 8-bit values to decimal (It's already in decimal in MATLAB);
 decimal_img = double(img);
 
 % Convert to grayscale
